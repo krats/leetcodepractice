59e5f265344e176f6f8113af32c8e7c2968035d3
Karthik Bashetty <karthiksathy@gmail.com>
2018-05-06 11:53:17 +0530

    Day2

--- src/main/java/com/karthikbashetty/leetcodepractice/ValidParentheses.java
+++ src/main/java/com/karthikbashetty/leetcodepractice/ValidParentheses.java
@@ -1,5 +1,31 @@
 package com.karthikbashetty.leetcodepractice;
 
+import java.util.Stack;
+
 public class ValidParentheses {
-    
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>();
+        for (int i = 0; i < s.length(); i++) {
+            char current = s.charAt(i);
+            if (current == '{' || current == '(' || current == '[') {
+                stack.push(current);
+            } else if (current == '}') {
+                if (stack.size() == 0 || stack.peek() != '{') {
+                    return false;
+                }
+                stack.pop();
+            } else if (current == ')') {
+                if (stack.size() == 0 || stack.peek() != '(') {
+                    return false;
+                }
+                stack.pop();
+            } else if (current == ']') {
+                if (stack.size() == 0 || stack.peek() != '[') {
+                    return false;
+                }
+                stack.pop();
+            }
+        }
+        return stack.size() == 0;
+    }
 }
